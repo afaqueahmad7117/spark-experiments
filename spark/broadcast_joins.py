58d42538564bcf21df0e0d79439c114ac5864657
Afaque Ahmad <linkedafaque@gmail.com>
2023-06-26 18:54:21 +0800

    feat: first commit
    Signed-off-by: Afaque Ahmad <linkedafaque@gmail.com>

---
+++ spark/broadcast_joins.py
@@ -0,0 +1,40 @@
+import time
+
+from pyspark.sql.types import StructType, IntegerType, StructField, StringType
+
+if __name__ == "__main__":
+    from pyspark.sql import SparkSession
+    spark = SparkSession.builder.getOrCreate()
+
+    # Approach 1
+
+    df_huge = spark.range(1, 10000000)
+
+    data = [
+        (1, "mocha"),
+        (1, "chai"),
+        (1, "latte"),
+    ]
+    schema = StructType([
+        StructField("id", IntegerType(), True),
+        StructField("name", StringType(), True),
+    ])
+    df_small = spark.createDataFrame(
+        data=data,
+        schema=schema
+    )
+
+    df_joined = df_huge.join(df_small, on="id", how="inner")
+    df_joined.explain()
+    start_time = time.time()
+    df_joined.show()
+    print(f"Time taken: {time.time() - start_time} seconds")
+
+    # Approach 2
+
+    import pyspark.sql.functions as F
+    df_broadcast_joined = df_huge.join(F.broadcast(df_small), on="id", how="inner")
+    df_broadcast_joined.explain()
+    start_time = time.time()
+    df_broadcast_joined.show()
+    print(f"Time taken: {time.time() - start_time} seconds")
---
+++ spark/partitioning.py
@@ -0,0 +1,41 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+
+
+def main():
+    spark = (
+        SparkSession
+        .builder
+        .appName("Spark Bucketing")
+        .enableHiveSupport()
+        .config("spark.driver.memory", "8g")
+        .getOrCreate()
+    )
+
+    spark.conf.set("spark.sql.autoBroadcastJoinThreshold", -1)
+
+    df_table_1 = spark.range(1, 1000000000)
+    df_table_2 = spark.range(1, 1000000)
+    df_table_1 = df_table_1.withColumn("test", F.lit(1))
+    df_table_2 = df_table_2.withColumn("test", F.lit(1))
+
+    df_table_1.repartition(5, "id").write.mode("overwrite").partitionBy("id").parquet("data/table_1.parquet")
+    df_table_2.repartition(5, "id").write.mode("overwrite").partitionBy("id").parquet("data/table_2.parquet")
+
+    print("done repartitioning")
+    df_joined = df_table_1.join(df_table_2, "id")
+    df_joined.explain()
+    print("done joining")
+
+    df_rep_table_1 = spark.read.parquet("data/table_1.parquet")
+    df_rep_table_2 = spark.read.parquet("data/table_2.parquet")
+    df_joined_2 = df_rep_table_1.join(df_rep_table_2, "id")
+    print("done joining 2")
+    df_joined_2.explain()
+
+    # to keep holding the spark ui
+    input()
+
+
+if __name__ == "__main__":
+    main()
